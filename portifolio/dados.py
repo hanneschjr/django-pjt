3b6989f431d34210c9407b7ed31964f42ac15801
Antonio Hannesch Junior <hanneschjr@gmail.com>
2026-03-25 17:07:24 -0300

    implementando  as páginas de projetos e detalhes do projeto

---
+++ portifolio/dados.py
@@ -0,0 +1,32 @@
+habilidades = {
+    'habilidade1': {
+        'nome': 'Python',
+    },
+    'habilidade2': {
+        'nome': 'Django',
+    },
+    'habilidade3': {
+        'nome': 'Flask'
+    }
+}
+
+projetos = {
+    'projeto1':{
+        'titulo': 'Portifólio',
+        'descricao': 'O primeiro projeto desenvolvido no Django',
+        'tecnologia': 'Python, Django, Git, GitHub', 
+        'link': 'https://github.com/hanneschjr/django-pjt',
+    },
+    'projeto2':{
+        'titulo': 'Jogo da velha',
+        'descricao': 'Um jogo simples desenvolvido apenas com o Python',
+        'tecnologia': 'Python', 
+        'link': 'https://github.com/hanneschjr/django-pjt',
+    },
+    'projeto3':{
+        'titulo': 'Oraculo',
+        'descricao': 'Um Chatbot que sabe tudo sobre a minha empresa',
+        'tecnologia': 'Python, Streamlit, LangChain', 
+        'link': 'https://github.com/hanneschjr/django-pjt',
+    }
+}
--- portifolio/urls.py
+++ portifolio/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('lista_projetos/', views.lista_projetos, name='lista_projetos'),
+    path('detalhes_projeto/<str:id_projeto>/', views.detalhes_projeto, name='detalhes_projeto'),
 ]
