7e8b3ca21b6db4211a6cff0996b6aba2910722fb
Antonio Hannesch Junior <hanneschjr@gmail.com>
2026-04-22 16:54:11 -0300

    Incluindo o banco de dados no projeto - adaptando o tamplate home.html

--- portifolio/models.py
+++ portifolio/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Habilidade(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return  self.nome
+    
+class Projeto(models.Model):
+    titulo = models.CharField(max_length=100)
+    decricao = models.TextField()
+    tecnologia = models.CharField(max_length=150)
+    github = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.titulo
--- portifolio/views.py
+++ portifolio/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from .dados import habilidades, projetos
+# from .dados import habilidades, projetos
+from .models import Habilidade, Projeto
 
 # Create your views here.
 def home(request):
+    habilidades = Habilidade.objects.all()
     return render(request, 'home.html', {'habilidades': habilidades})
 
 def lista_projetos(request):
+    projetos = Projeto.objects.all()
     return render(request, 'projetos.html', {'projetos': projetos})
 
 def detalhes_projeto(request, id_projeto):
-    projeto = projetos.get(id_projeto)
+
+    projeto = Projeto.get(id=id_projeto)
     infos = projeto.get('tecnologia').split(', ')
     return render(request, 'detalhes_projeto.html', {'projeto': projeto, 'infos': infos})
