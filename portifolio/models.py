af8abe24be059ea73141dd84e6c8fa7dd386e6e8
Antonio Hannesch Junior <hanneschjr@gmail.com>
2026-04-23 16:52:57 -0300

    implementando o model para icones

--- portifolio/models.py
+++ portifolio/models.py
@@ -3,10 +3,12 @@ from django.db import models
 # Create your models here.
 class Habilidade(models.Model):
     nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    icone = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return  self.nome
-    
+                
 class Projeto(models.Model):
     titulo = models.CharField(max_length=100)
     decricao = models.TextField()
@@ -15,3 +17,10 @@ class Projeto(models.Model):
 
     def __str__(self):
         return self.titulo
+
+class icone(models.Model):
+    nome = models.CharField(max_length=50)
+    desenho = models.TextField(blanck=True, null=True)
+
+    def __str__(self):
+        return self.nome
--- portifolio/views.py
+++ portifolio/views.py
@@ -12,7 +12,6 @@ def lista_projetos(request):
     return render(request, 'projetos.html', {'projetos': projetos})
 
 def detalhes_projeto(request, id_projeto):
-
     projeto = Projeto.get(id=id_projeto)
     infos = projeto.get('tecnologia').split(', ')
     return render(request, 'detalhes_projeto.html', {'projeto': projeto, 'infos': infos})
